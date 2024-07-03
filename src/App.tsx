16db81f2ae486a140fb867aafa01b7d905c97f09
wamuyu <yvonnewamuyug@gmail.com>
2024-07-03 10:20:33 +0300

    configure ACL

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from "react";
-import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+interface ShipInterface {
+  name: string;
+  home_port: string;
+  image: string;
+}
+
+const SPACE_X_LAUNCHES = gql`
+  query GetSpaceXlaunches {
+    launchesPast(limit: 10) {
+      ships {
+        name
+        home_port
+        image
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(SPACE_X_LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log("data", data);
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <ul>
+      {data.launchesPast.map((launch: any) =>
+        launch.ships.map((ship: ShipInterface) => <li>{ship.name}</li>)
+      )}
+    </ul>
   );
 }
 
